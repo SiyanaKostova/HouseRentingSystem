5412c03e36d678a896e5cf28960a7efaac2b989e
Siyana Kostova <siyana.kostova06@abv.bg>
2024-03-05 21:52:14 +0200

    Names of async methods fixed.

--- HouseRentingSystem.Core/Services/AgentService.cs
+++ HouseRentingSystem.Core/Services/AgentService.cs
@@ -1,13 +1,38 @@
 ﻿using HouseRentingSystem.Core.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using HouseRentingSystem.Infrastructure.Common;
+using HouseRentingSystem.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace HouseRentingSystem.Core.Services
 {
 	public class AgentService : IAgentService
 	{
+		private readonly IRepository repository;
+
+        public AgentService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+		public Task CreateAsync(string userId, string phoneNumber)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<bool> ExistsByIdAsync(string userId)
+		{
+			return await repository.AllReadOnly<Agent>()
+				.AnyAsync(a => a.UserId == userId);
+		}
+
+		public Task<bool> UserHasRentsAsync(string userId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> UserWithPhoneNumberExistsAsync(string phoneNumber)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
