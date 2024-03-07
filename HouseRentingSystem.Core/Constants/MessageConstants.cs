2d05e0564280cc000276cdf412f4d41d9c70f18e
Siyana Kostova <siyana.kostova06@abv.bg>
2024-03-07 21:02:50 +0200

    Added new message constants for agent phone number and rent

--- HouseRentingSystem.Core/Constants/MessageConstants.cs
+++ HouseRentingSystem.Core/Constants/MessageConstants.cs
@@ -5,5 +5,9 @@
 		public const string RequiredMessage = "The {0} field is required";
 
 		public const string LengthMessage = "The {0} field must be between {2} and {1} characters long";
+
+		public const string PhoneExistsMessage = "Phone number already exists. Enter another one";
+
+		public const string NoRentsMessage = "You should have no rents to become an agent";
 	}
 }
