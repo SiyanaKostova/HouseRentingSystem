df821feba7b55d713d59a8fd77d36463099992d4
Siyana Kostova <siyana.kostova06@abv.bg>
2024-03-07 21:05:37 +0200

    Become an Agent implemented in controller

--- HouseRentingSystem/Controllers/AgentController.cs
+++ HouseRentingSystem/Controllers/AgentController.cs
@@ -1,7 +1,9 @@
-﻿using HouseRentingSystem.Core.Contracts;
+﻿using HouseRentingSystem.Attributes;
+using HouseRentingSystem.Core.Contracts;
 using HouseRentingSystem.Core.Models.Agent;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using static HouseRentingSystem.Core.Constants.MessageConstants;
 
 namespace HouseRentingSystem.Controllers
 {
@@ -15,21 +17,35 @@ namespace HouseRentingSystem.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Become()
+        [NotAnAgent]
+        public IActionResult Become()
         {
-            if (await agentService.ExistsByIdAsync(User.Id()))
-            {
-                return BadRequest();
-            }
-
             var model = new BecomeAgentFormModel();
 
             return View(model);
         }
 
 		[HttpPost]
-		public async Task<IActionResult> Become(BecomeAgentFormModel agent)
+        [NotAnAgent]
+		public async Task<IActionResult> Become(BecomeAgentFormModel model)
 		{
+            if (await agentService.UserWithPhoneNumberExistsAsync(User.Id()))
+            {
+                ModelState.AddModelError(nameof(model.PhoneNumber), PhoneExistsMessage);
+            }
+
+            if (await agentService.UserHasRentsAsync(User.Id()))
+            {
+				ModelState.AddModelError("Error", NoRentsMessage);
+			}
+
+            await agentService.CreateAsync(User.Id(), model.PhoneNumber);
+
+			if (ModelState.IsValid == false)
+            {
+                return View(model);
+            }
+
             return RedirectToAction(nameof(HouseController.All), "Houses");
 		}
 	}
