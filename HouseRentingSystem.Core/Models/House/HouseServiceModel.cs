2598b57b9bba9b48ddaba2aa363475f8f66f9f9e
Siyana Kostova <siyana.kostova06@abv.bg>
2024-03-14 22:39:33 +0200

    Created new models for house query.

---
+++ HouseRentingSystem.Core/Models/House/HouseServiceModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Infrastructure.Constants.DataConstants;
+using static HouseRentingSystem.Core.Constants.MessageConstants;
+
+namespace HouseRentingSystem.Core.Models.House
+{
+    public class HouseServiceModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(HouseTitleMaxLength,
+        MinimumLength = HouseTitleMinLength,
+        ErrorMessage = LengthMessage)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(HouseAddressMaxLength,
+            MinimumLength = HouseAddressMinLength,
+            ErrorMessage = LengthMessage)]
+        public string Address { get; set; } = string.Empty;
+
+        [DisplayName("Image URL")]
+        [Required(ErrorMessage = RequiredMessage)]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [Range(typeof(decimal),
+            HousePricePerMonthMinLength,
+            HousePricePerMonthMaxLength,
+            ConvertValueInInvariantCulture = true,
+            ErrorMessage = "Price per month must be a positive number and less than {2} leva")]
+        [DisplayName("Price Per Month")]
+        public decimal PricePerMonth { get; set; }
+
+        [DisplayName("Is Rented")]
+        public bool IsRented { get; set; }
+    }
+}
