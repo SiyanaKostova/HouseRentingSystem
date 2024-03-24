4b5027806576769ead5c7e98ed6813779f153614
Siyana Kostova <siyana.kostova06@abv.bg>
2024-03-24 17:51:56 +0200

    Model for House Title and Addres added.

---
+++ HouseRentingSystem.Core/Contracts/IHouseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HouseRentingSystem.Core.Contracts
+{
+    public class IHouseModel
+    {
+        public string Title { get; set; }
+
+        public string Address { get; set; }
+    }
+}
