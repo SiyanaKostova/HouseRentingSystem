d3641a74a45f9b150a4c66e152ed0e4f23708d48
Siyana Kostova <siyana.kostova06@abv.bg>
2024-04-06 21:11:40 +0300

    House Controller added to Admin Area  and injected House and Agent Services.

---
+++ HouseRentingSystem/Areas/Admin/Controllers/HouseController.cs
@@ -0,0 +1,25 @@
+﻿using HouseRentingSystem.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Areas.Admin.Controllers
+{
+    public class HouseController : AdminBaseController
+    {
+        private readonly IHouseService houseService;
+
+        private readonly IAgentService agentService;
+
+        public HouseController(
+            IHouseService _houseService,
+            IAgentService _agentService)
+        {
+            houseService = _houseService;
+            agentService = _agentService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
